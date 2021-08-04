4344bf06302988f06be807cbc325929925501177
Jacob <51940299+2xjtn@users.noreply.github.com>
2021-08-04 11:06:56 -0700

    asdas
    asdasd

--- Docker/src/main/java/org/oxygen/App.java
+++ Docker/src/main/java/org/oxygen/App.java
@@ -14,37 +14,38 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class App {
 
-    public static void main(String[] args) {
-        DockerClientConfig config = DefaultDockerClientConfig.createDefaultConfigBuilder().build();
+    public static void main() {
+        System.out.println("Nigger");
+//         DockerClientConfig config = DefaultDockerClientConfig.createDefaultConfigBuilder().build();
 
-        DockerHttpClient client = new ApacheDockerHttpClient.Builder()
-                .dockerHost(config.getDockerHost())
-                .sslConfig(config.getSSLConfig())
-                .build();
+//         DockerHttpClient client = new ApacheDockerHttpClient.Builder()
+//                 .dockerHost(config.getDockerHost())
+//                 .sslConfig(config.getSSLConfig())
+//                 .build();
 
-        DockerHttpClient.Request request = DockerHttpClient.Request.builder()
-                .method(DockerHttpClient.Request.Method.GET)
-                .path("/containers/json")
-                .build();
+//         DockerHttpClient.Request request = DockerHttpClient.Request.builder()
+//                 .method(DockerHttpClient.Request.Method.GET)
+//                 .path("/containers/json")
+//                 .build();
 
-        try (DockerHttpClient.Response response = client.execute( request )) {
-            assertThat(response.getStatusCode(), Matchers.equalTo(200));
-            String a = IOUtils.toString( response.getBody(), Charset.defaultCharset() );
-            JSONArray jsonArray = new JSONArray(a);
+//         try (DockerHttpClient.Response response = client.execute( request )) {
+//             assertThat(response.getStatusCode(), Matchers.equalTo(200));
+//             String a = IOUtils.toString( response.getBody(), Charset.defaultCharset() );
+//             JSONArray jsonArray = new JSONArray(a);
 
-            int i;
+//             int i;
 
-            for(i = 0; i < jsonArray.length(); i++) {
-                System.out.println( "Id: " + jsonArray.getJSONObject( i ).get( "Id" ) );
-                System.out.println("Names: " + jsonArray.getJSONObject( i ).get( "Names" ));
-            }
+//             for(i = 0; i < jsonArray.length(); i++) {
+//                 System.out.println( "Id: " + jsonArray.getJSONObject( i ).get( "Id" ) );
+//                 System.out.println("Names: " + jsonArray.getJSONObject( i ).get( "Names" ));
+//             }
 
-            if(i == 0) {
-                System.out.println("No docker containers found");
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+//             if(i == 0) {
+//                 System.out.println("No docker containers found");
+//             }
+//         } catch (Exception e) {
+//             e.printStackTrace();
+//         }
 
     }
 
